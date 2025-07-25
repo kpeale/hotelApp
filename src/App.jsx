5eccd38899667756605ec892553141fa0b60784d
Kpeale Legbara <66691866+kpeale@users.noreply.github.com>
2025-07-25 04:47:15 +0100

    I finished working on the hero page for the hotel booking platform

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
-
+import React from 'react';
+import Navbar from './components/Navbar';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
-  
-
+  const isOwnerPath = useLocation().pathname.includes('owner');
   return (
-    
-      <div>
-        
-      <h1>Vite + React</h1>
+    <>
+      {!isOwnerPath && <Navbar />}
+      <div className='min-h-[70vh]'>
+        <Routes>
+          <Route
+            path='/'
+            element={<Home />}
+          />
+        </Routes>
       </div>
-      
-   
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
